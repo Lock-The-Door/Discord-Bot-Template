ab88ba54f58ee7628e593bc6be58986b38a7e2cf
Lock-The-Door <49077851+Lock-The-Door@users.noreply.github.com>
2019-07-14 22:35:30 -0400

    Initial commit

---
+++ Discord Bot Template/Commands.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Discord_Bot_Template
+{
+    public class TestModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        [Summary("A test command")]
+        public async Task Test()
+        {
+            await Context.Channel.SendMessageAsync("test");
+        }
+
+        [Command("inputTest")]
+        [Summary("tests input by sending a echo")]
+        public async Task InputTest(string input)
+        {
+            await Context.Channel.SendMessageAsync(input);
+        }
+    }
+}
---
+++ Discord Bot Template/Login.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Discord_Bot_Template
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
